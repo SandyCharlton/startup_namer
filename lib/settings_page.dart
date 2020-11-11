b15a11bc0a2dd96ccaecd07fb33195f9bdf36c7a
SandyCharlton <sandy.charlton.1983@gmail.com>
2020-11-10 19:48:21 -0800

    Branched out to a new method for Dark mode implementation

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -1,191 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:startup_namer/theme_changer.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-// this contains all the code for our settings page
-
-
-// Creates the settings page
-class SettingsPage extends StatefulWidget {
+class SettingsPage extends StatelessWidget {
   @override
-  _SettingsPageState createState() => _SettingsPageState();
-}
-
-class _SettingsPageState extends State<SettingsPage> {
-  bool isDarkModeBool = false;
-  bool isSandyRad = false;
-
-  // These are the bools for metrics
-  bool isSpeedOn = true;
-  bool isSpeedAvgOn = true;
-  bool isDistanceOn = true;
-  bool isTimeOn = true;
-  bool isStrokeRateOn = true;
-  bool isStrokeRateAvgOn = true;
-
-  @override
-  void initState() {
-    super.initState();
-    getSwitchValues();
-  }
-
-  getSwitchValues() async {
-    isDarkModeBool = await getSwitchState();
-    setState(() {
-
-    });
-  }
-
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Settings'),
+          title: Text("Theme Switcher"),
         ),
-        // TODO: Change the location of the button to be in a list.
-        body: Align(
-            alignment: Alignment.topLeft, // starts this whole container in the top left corner
-            child: Container(
-                padding: EdgeInsets.all(20),
-                child: Column( // This container holds the entire screen. All the rows, all the buttons, everything
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Row( // This row is for dark mode selection
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text ('DarkMode', style: TextStyle(fontSize: (20))),
-                        Switch(
-                          value: isDarkModeBool,
-                          onChanged: (value) {
-                            ThemeBuilder.of(context).changeTheme();
-                            setState(() {
-                              isDarkModeBool = value;
-                            });
-                          },
-                          activeTrackColor: Colors.lightGreenAccent,
-                          activeColor: Colors.green,
-                        )
-                      ],
-                    ),
-                    Row( // This row asks if Sandy is Rad? At the time, it was a test to learn how to add a second button
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children:[
-                        Text ('Is Sandy Rad?', style: TextStyle(fontSize: (20))),
-                        Switch(
-                          value: isSandyRad,
-                          onChanged: (value) {
-                            setState(() {
-                              isSandyRad = value;
-                              print(isSandyRad);
-                            });
-                          },
-                          activeTrackColor: Colors.lightGreenAccent,
-                          activeColor: Colors.green,
-                        )
-                      ]
-                    ),
-                    Divider( // divides the app with a thin blue line
-                      thickness: 1,
-                      color: Colors.blueAccent,
-                    ),
-                    SizedBox( // this sized box is used to take up space, nothing more
-                      height: 10,
-                    ),
-                    Text( // a small piece of text that
-                      'Paddler Modules', style: TextStyle(fontSize: (14), color: Colors.blue)
-                    ),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children:[
-                          Text ('Show Speed?', style: TextStyle(fontSize: (20))),
-                          Checkbox(
-                            value: isSpeedOn,
-                            onChanged: (value) {
-                              setState(() {
-                                isSpeedOn = value;
-                                print(isSpeedOn);
-                              });
-                            },
-                          )
-                        ]
-                    ),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children:[
-                          Text ('Show Average Speed?', style: TextStyle(fontSize: (20))),
-                          Checkbox(
-                            value: isSpeedAvgOn,
-                            onChanged: (value) {
-                              setState(() {
-                                isSpeedAvgOn = value;
-                                print(isSpeedAvgOn);
-                              });
-                            },
-                          )
-                        ]
-                    ),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children:[
-                          Text ('Show Distance?', style: TextStyle(fontSize: (20))),
-                          Checkbox(
-                            value: isDistanceOn,
-                            onChanged: (value) {
-                              setState(() {
-                                isDistanceOn = value;
-                                print(isDistanceOn);
-                              });
-                            },
-                          )
-                        ]
-                    ),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children:[
-                          Text ('Show Time?', style: TextStyle(fontSize: (20))),
-                          Checkbox(
-                            value: isTimeOn,
-                            onChanged: (value) {
-                              setState(() {
-                                isTimeOn = value;
-                                print(isTimeOn);
-                              });
-                            },
-                          )
-                        ]
-                    ),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children:[
-                          Text ('Show Stroke Rate?', style: TextStyle(fontSize: (20))),
-                          Checkbox(
-                            value: isStrokeRateOn,
-                            onChanged: (value) {
-                              setState(() {
-                                isStrokeRateOn = value;
-                                print(isStrokeRateOn);
-                              });
-                            },
-                          )
-                        ]
-                    ),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children:[
-                          Text ('Show Average Stroke Rate?', style: TextStyle(fontSize: (20))),
-                          Checkbox(
-                            value: isStrokeRateAvgOn,
-                            onChanged: (value) {
-                              setState(() {
-                                isStrokeRateAvgOn = value;
-                                print(isStrokeRateAvgOn);
-                              });
-                            },
-                          )
-                        ]
-                    ),
-                  ],
-                )
-            )
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SwitchListTile(
+                  title: Text("Dark Mode"),
+                  onChanged:(value){
+                  } ,
+                  value:false ,
+                ),
+                Card(
+                  child: ListTile(
+                    title: Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit."),
+                  ),
+                ),
+              ]
+          ),
         )
     );
   }
--- lib/theme_changer.dart
+++ lib/theme_changer.dart
@@ -3,38 +3,18 @@ import 'package:flutter/material.dart';
 // this class will be used to switch themes at the push of a button.
 // taken from https://www.youtube.com/watch?v=WMvjL6AN9dY
 
-// most of this stuff is setup automatically when creating a Stateful Widget
-class ThemeBuilder extends StatefulWidget {
-  final Widget Function(BuildContext context, Brightness brightness) builder;
-  ThemeBuilder({this.builder});
-  @override
-  _ThemeBuilderState createState() => _ThemeBuilderState();
-  static _ThemeBuilderState of (BuildContext context)
-  {
-    return context.ancestorStateOfType(const TypeMatcher<_ThemeBuilderState>()); // this method is depreciated.
-    // TODO: There's probably a better way to do this, but for now, this works.
-  }
-}
+// Revised completely on November 10th, 2020
+// https://codesource.io/building-theme-switcher-using-provider-and-shared-preferences/
 
-class _ThemeBuilderState extends State<ThemeBuilder> {
-  Brightness _brightness;
+ThemeData light = ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.indigo,
+    accentColor: Colors.pink,
+    scaffoldBackgroundColor: Color(0xfff1f1f1)
+);
 
-  @override
-  // The initial state of the app. Currently it's set to light, but you can change that
-  void initState() {
-    super.initState();
-    _brightness = Brightness.light;
-  }
-
-  void changeTheme() {
-    setState(() {
-      _brightness =
-          _brightness == Brightness.dark ? Brightness.light : Brightness.dark;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return widget.builder(context, _brightness);
-  }
-}
+ThemeData dark = ThemeData(
+  brightness: Brightness.dark,
+  primarySwatch: Colors.indigo,
+  accentColor: Colors.pink,
+);
